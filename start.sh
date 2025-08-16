8f25093ec977e28a9f19ea8c716539bdf2f655b5
Ishatgrepo <dmcomdot@gmail.com>
2025-08-16 19:14:41 +0000

    start change

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-source .venv/bin/activate && python3 update.py && python3 -m bot && python3 HoneyTdataBot/main.py && python3 extracterbot/
-bot.py
+source .venv/bin/activate && python3 update.py && python3 -m bot 
+python3 HoneyTdataBot/main.py && python3 extracterbot/bot.py
